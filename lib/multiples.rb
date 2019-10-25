cf829487080370fd3890a0231552a706f38a5629
gnardinosaur <github email address>
2019-10-25 16:52:54 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+    divisible_nums = []
+    range = (1...limit).to_a
+    range.each do |el|
+        if el % 3 == 0 || el % 5 == 0
+            divisible_nums << el
+        end
+    end
+    divisible_nums
+end
+
+def sum_multiples(limit)
+    divisible_nums_arr = collect_multiples(limit)
+    divisible_nums_arr.sum
+end
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
-# Enter your object-oriented solution here!
+class Multiples 
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        divisible_nums = []
+        range = (1...limit).to_a
+        range.each do |el|
+            if el % 3 == 0 || el % 5 == 0
+                divisible_nums << el
+            end
+        end
+        divisible_nums
+    end
+    
+    def sum_multiples
+        divisible_nums_arr = collect_multiples
+        divisible_nums_arr.sum
+    end    
+
+end
